9616049247b8395dc3e2aa90125b6684cfaf7384
arkash55 <arkash.vijay55@gmail.com>
2025-08-03 19:40:42 +0100

    configured firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0vDkwuLDW1j4fXVltAmHZPY6-M3gPx8k",
+  authDomain: "talka-c5df4.firebaseapp.com",
+  projectId: "talka-c5df4",
+  storageBucket: "talka-c5df4.firebasestorage.app",
+  messagingSenderId: "750480431215",
+  appId: "1:750480431215:web:b2cd4d2e7c130bf931d59a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
