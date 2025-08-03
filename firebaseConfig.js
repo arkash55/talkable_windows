b150096834d40569184b572f8b9d7e7d68caa7bf
arkash55 <arkash.vijay55@gmail.com>
2025-08-03 20:13:16 +0100

    add auth import/export

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyA0vDkwuLDW1j4fXVltAmHZPY6-M3gPx8k",
   authDomain: "talka-c5df4.firebaseapp.com",
@@ -13,5 +13,6 @@ const firebaseConfig = {
   appId: "1:750480431215:web:b2cd4d2e7c130bf931d59a"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
