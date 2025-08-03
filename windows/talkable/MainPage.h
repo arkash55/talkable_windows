50afd784445955ba13dcc7d82c0047683d4084fc
arkash55 <arkash.vijay55@gmail.com>
2025-08-03 14:30:43 +0100

    added remaining files

---
+++ windows/talkable/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::talkable::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::talkable::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
