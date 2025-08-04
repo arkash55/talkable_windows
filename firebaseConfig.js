d028d957229592c1e68e4bdeb3e251e6f4a227b7
arkash55 <arkash.vijay55@gmail.com>
2025-08-04 13:48:59 +0100

    created .env and added to ignore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,23 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { 
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0vDkwuLDW1j4fXVltAmHZPY6-M3gPx8k",
-  authDomain: "talka-c5df4.firebaseapp.com",
-  projectId: "talka-c5df4",
-  storageBucket: "talka-c5df4.firebasestorage.app",
-  messagingSenderId: "750480431215",
-  appId: "1:750480431215:web:b2cd4d2e7c130bf931d59a"
+   apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 
